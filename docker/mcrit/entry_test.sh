90cbf4f5bf2163aa568bf8b42b48f923d99bc6a1
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2023-04-26 13:55:07 +0200

    automatically build/test the image using github workflows

---
+++ docker/mcrit/entry_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+# wait for mongodb
+#until $(curl --noproxy '*' --output /dev/null --head --fail http://mongodb:27017/); do
+#    echo "Waiting for mongodb..."
+#    sleep 1
+#done
+sleep 1
+cd /opt/mcrit
+make test-nomongo
