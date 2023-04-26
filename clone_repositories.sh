c97dac090756fe1f04402f5e579035ff25b58253
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2023-04-26 13:20:55 +0200

    modified clone to now point to proper repo

--- clone_repositories.sh
+++ clone_repositories.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-git clone https://github.com/danielplohmann/mcritweb.git ./repositories/mcritweb
+git clone https://github.com/fkie-cad/mcritweb.git ./repositories/mcritweb
 git clone https://github.com/danielplohmann/mcrit.git ./repositories/mcrit
