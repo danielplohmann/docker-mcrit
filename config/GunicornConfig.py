df322f7bd69291590ea09f5512bfa93f8c69175a
pnx@pyrite <daniel.plohmann@fkie.fraunhofer.de>
2023-09-13 15:12:35 +0200

    docker-mcrit with gunicorn deployment

---
+++ config/GunicornConfig.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+from mcrit.config.ConfigInterface import ConfigInterface
+
+@dataclass
+class GunicornConfig(ConfigInterface):
+    # specified as "<host>:<port>" to be able to pass it easily into gunicorn config
+    BIND: str = "0.0.0.0:8000"
+    # The number of workers gunicorn will spin up
+    WORKERS: int = 4
+    # The number of threads in each worker
+    THREADS: int = 8
+    # timeout before silent workers are killed
+    TIMEOUT: int = 120
