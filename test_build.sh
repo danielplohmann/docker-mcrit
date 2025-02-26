e3875389bac0fdae4951ab25cb0ebed67fc20356
Daniel Plohmann (Saturn) <daniel.plohmann@mailbox.org>
2025-02-26 12:18:35 +0100

    bump ubuntu to 22.04

--- test_build.sh
+++ test_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env set
 echo "building mcrit ${MCRIT_BRANCH} / mcritweb ${MCRITWEB_BRANCH} (mcrit tag: ${MCRIT_TAG})"
-docker build -t mcrit docker/mcrit --build-arg MCRIT_BRANCH=${MCRIT_BRANCH}
-docker build -t mcritweb docker/mcritweb --build-arg MCRITWEB_BRANCH=${MCRITWEB_BRANCH} --build-arg MCRIT_TAG=${MCRIT_TAG}
+docker build -t mcrit docker/mcrit --no-cache --build-arg MCRIT_BRANCH=${MCRIT_BRANCH}
+docker build -t mcritweb docker/mcritweb --no-cache --build-arg MCRITWEB_BRANCH=${MCRITWEB_BRANCH} --build-arg MCRIT_TAG=${MCRIT_TAG}
