90733748126dfae5da6063892d84484719aeeaef
Daniel Plohmann (apatite) <daniel.plohmann@mailbox.org>
2025-11-17 16:46:44 +0100

    upgrade clone repo script to alternatively clone or update

--- clone_repositories.sh
+++ clone_repositories.sh
@@ -1,3 +1,15 @@
-#/bin/bash
-git clone https://github.com/fkie-cad/mcritweb.git ./repositories/mcritweb
-git clone https://github.com/danielplohmann/mcrit.git ./repositories/mcrit
+#!/bin/bash
+
+# Clone or update mcritweb
+if [ ! -d "./repositories/mcritweb" ]; then
+    git clone https://github.com/fkie-cad/mcritweb.git ./repositories/mcritweb
+else
+    cd ./repositories/mcritweb && git pull && cd ../..
+fi
+
+# Clone or update mcrit
+if [ ! -d "./repositories/mcrit" ]; then
+    git clone https://github.com/danielplohmann/mcrit.git ./repositories/mcrit
+else
+    cd ./repositories/mcrit && git pull && cd ../..
+fi
